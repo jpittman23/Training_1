2bdd648a7d6f9d2562a768c7da88ca90e6c979b1
jordan pittman <jpittman306@gmail.com>
2020-07-15 15:31:34 -0500

    add hello csharp project

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿//using c = System.Console; - alias
+//using System;
+//
+//namespace HelloCsharp //directory
+//{
+//    class Program
+//    {
+//        static void Main(string[] args) //Pascal Casing - first letter of every word must be capitalized
+//        {
+//            Console.WriteLine("Hello World!");
+//            HelloCsharp.Program.Main("");
+ //       }
+//    }
+//}
+using System;
 
 namespace HelloCsharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter Firstname: ");
+            var firstName = Console.ReadLine(); //Lazy, datatype given based on content of Readline
+            Console.Write("Enter Lastname: ");
+            string lastName = Console.ReadLine(); //Eager, datatype is explicit pior to content of readline
+
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine("{0} {1}", firstName, lastName);
+            Console.WriteLine($"{firstName} {lastName}");
+
+            Console.ReadLine();
         }
     }
 }
